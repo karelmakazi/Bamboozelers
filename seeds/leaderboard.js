7ccf97c007308f56e975fce98e5a7bd61ec8022b
Karel Fourie <karelmakazi@gmail.com>
2020-05-26 09:20:39 +1200

    Update seed

--- seeds/leaderboard.js
+++ seeds/leaderboard.js
@@ -5,8 +5,10 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('leaderboard').insert([
-        { score_id: 1, name: 'Alicia', category: 'Animals', score: 10 },
-        { score_id: 2, name: 'John', category: 'Film', score: 8 },
+        { score_id: 1, name: 'Alicia', category: 'Animals', score: 2 },
+        { score_id: 2, name: 'John', category: 'Films', score: 2 },
+        { score_id: 3, name: 'Hamish', category: 'Science', score: 2 },
+        { score_id: 4, name: 'Karel', category: 'History', score: 2 },
       ])
     });
 };
